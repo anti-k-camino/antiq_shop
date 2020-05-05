53ae9619a491a6e9b85f754542a3f2b69b4c6884
antiq <kratyukan@gmail.com>
2020-05-05 13:59:02 +0300

    pug added

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
+// const path = require('path');
 
-const port = process.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('main', {
+    foo: 4,
+    bar: 7
+  });
+});
+
+app.get('/cat', (req, res) => {
+  res.send('cat');
+});
+// app.get("/cat", (req, res) => {
+//   console.log("Running...");
+//   // res.end('The end');
+//   return res.render("index.html");
+// });
 
 app.listen(port, () => console.log(`Listening  on port ${port}...`));

e32a063efcd815cf2580537d5b0895cc50933c9b
antiq <kratyukan@gmail.com>
2020-06-06 20:01:59 +0300

    views performed. a single good page

--- index.js
+++ index.js
@@ -9,23 +9,26 @@ app.set('views', 'antiq_shop/views'); // just for dev !!!
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  getGoods().then( goods => res.render('main', { goods }) );
+  getGoods()
+    .then( goods => res.render('main', { goods }) );
+});
+
+app.get('/goods', (req, res) => {
+  const goodId = req.query.id;
+  if (!goodId) // validate query
+    return res.status(400).send('Bad request, malformed URL.');
+  getGood(goodId)
+    .then(good => {
+      return res.render('good', { good });
+    });
 });
 
 app.get('/category', (req, res) => {
   const catId = req.query.id;
   if (!catId) // validate req
-    return res.status(400).send('Malformed URL. Bad request.');
-  const category = new Promise((resolve, reject) => {
-    connection
-      .query(`SELECT * FROM category WHERE id = ${catId}`, (err, catArr) => {
-        if (err) reject(err);
-        resolve(catArr[0]); // resolves pure object
-      });
-  });
-  Promise.all([category, getGoods(catId)])
+    return res.status(400).send('Bad request, malformed URL.');
+  Promise.all([getCategory(catId), getGoodsByCat(catId)])
     .then(val => {
-      console.log(val[1]);
       return res.render('category', {
         category: val[0],
         goods: val[1]
@@ -33,22 +36,51 @@ app.get('/category', (req, res) => {
     });
 });
 
-function getGoods (key=0) {
-  let queryString =  '';
-  if (key === 0) {
-    queryString = 'SELECT * FROM goods'
-  } else {
-    queryString = `SELECT * FROM goods WHERE category = ${key}`
-  }
+function getGoods () {
   return new Promise((resolve, reject) => {
     connection
-      .query(queryString, (err, goodsArr) => {
+      .query('SELECT * FROM goods', (err, goodsArr) => {
         if (err) reject(err);
         const goodsObj = {};
         for(let i = 0; i < goodsArr.length; ++i){
           goodsObj[goodsArr[i]['id']] = goodsArr[i];
         }
         resolve(goodsObj);
+      });
+  });
+};
+
+function getGoodsByCat (catId) {
+  return new Promise((resolve, reject) => {
+    connection
+      .query(`SELECT * FROM goods WHERE category = ${catId}`, (err, goodsArr) => {
+        if (err) reject(err);
+        const goodsObj = {};
+        for(let i = 0; i < goodsArr.length; ++i){
+          goodsObj[goodsArr[i]['id']] = goodsArr[i];
+        }
+        resolve(goodsObj);
+      });
+  });
+};
+
+function getCategory (catId) {
+  return new Promise((resolve, reject) => {
+    connection
+      .query(`SELECT * FROM category WHERE id = ${catId}`, (err, catArr) => {
+        if (err) reject(err);
+        // if (!catArr[0]) reject({ message: 'Not Found' , status: 404 }); !! Provide this validation !!
+        resolve(catArr[0]); // resolves pure object
+      });
+  });
+};
+
+function getGood (goodId) {
+  return new Promise((resolve, reject) => {
+    connection
+      .query(`SELECT * FROM goods WHERE id = ${goodId}`, (err, goodArr) => {
+        if (err) reject(err);
+        resolve(goodArr[0]);
       });
   });
 };
